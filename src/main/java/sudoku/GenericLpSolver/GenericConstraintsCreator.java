77a9b2d425c8fed942c4a0d034cdbc6030948b4e
morshka <mor.sheffer@gmail.com>
2015-05-21 23:09:51 +0300

    Performance

--- src/main/java/sudoku/GenericLpSolver/GenericConstraintsCreator.java
+++ src/main/java/sudoku/GenericLpSolver/GenericConstraintsCreator.java
@@ -9,61 +9,29 @@ public class GenericConstraintsCreator {
     public List<Constraint> create(){
 
         List<Constraint> constraints = new ArrayList<>();
-        constraints.addAll(findVariablesForCellConstraints());
-        constraints.addAll(findVariablesForRowConstraints());
-        constraints.addAll(findVariablesForColumnConstraints());
+        constraints.addAll(findVariablesForCellAndRowsAndColumnsConstraints());
         constraints.addAll(findVariablesForSquareConstraints());
         return constraints;
     }
 
-    private List<Constraint> findVariablesForCellConstraints() {
+    private List<Constraint> findVariablesForCellAndRowsAndColumnsConstraints() {
         List<Constraint> constraints = new ArrayList<>(); //we should have 9 per cell (total 81)
 
         for (int i = 0; i < 9; i++) {
             // we need to create 9 constraints per iteration
             for (int j = 0; j < 9; j++) {
                 // one constraint
-                List<Integer> variables = new ArrayList<>();
+                List<Integer> variablesForCell = new ArrayList<>();
+                List<Integer> variablesForRow = new ArrayList<>();
+                List<Integer> variablesForColumn = new ArrayList<>();
                 for (int k = 0; k <9; k++) {
-                    variables.add(i*81 + j*9 + k);
+                    variablesForCell.add(i*81 + j*9 + k);
+                    variablesForRow.add(i*81 + k*9 + j);
+                    variablesForColumn.add(k*81 + i*9 + j);
                 }
-                constraints.add(new Constraint(variables, 1));
-            }
-        }
-
-        return constraints;
-    }
-
-    private List<Constraint> findVariablesForRowConstraints() {
-        List<Constraint> constraints = new ArrayList<>(); //we should have 9 per row (total 81)
-
-        for (int i = 0; i < 9; i++) {
-            // we need to create 9 constraints per iteration
-            for (int j = 0; j < 9; j++) {
-                // one constraint
-                List<Integer> variables = new ArrayList<>();
-                for (int k = 0; k <9; k++) {
-                    variables.add(i*81 + k*9 + j);
-                }
-                constraints.add(new Constraint(variables, 1));
-            }
-        }
-
-        return constraints;
-    }
-
-    private List<Constraint> findVariablesForColumnConstraints() {
-        List<Constraint> constraints = new ArrayList<>(); //we should have 9 per column (total 81)
-
-        for (int i = 0; i < 9; i++) {
-            // we need to create 9 constraints per iteration
-            for (int j = 0; j < 9; j++) {
-                // one constraint
-                List<Integer> variables = new ArrayList<>();
-                for (int k = 0; k <9; k++) {
-                    variables.add(k*81 + i*9 + j);
-                }
-                constraints.add(new Constraint(variables, 1));
+                constraints.add(new Constraint(variablesForCell, 1));
+                constraints.add(new Constraint(variablesForRow, 1));
+                constraints.add(new Constraint(variablesForColumn, 1));
             }
         }
 
